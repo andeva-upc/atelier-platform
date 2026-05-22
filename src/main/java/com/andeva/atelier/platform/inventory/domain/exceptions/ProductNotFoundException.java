f022bf5cc67b7b6eb43817b94e7a7085dc86fa83
xs4el <adiel.sanchez3110@gmail.com>
2026-05-22 16:20:00 -0500

    feat(inventory): add product not found exception

---
+++ src/main/java/com/andeva/atelier/platform/inventory/domain/exceptions/ProductNotFoundException.java
@@ -0,0 +1,4 @@
+﻿package com.andeva.atelier.platform.inventory.domain.exceptions;
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) { super(message); }
+}
