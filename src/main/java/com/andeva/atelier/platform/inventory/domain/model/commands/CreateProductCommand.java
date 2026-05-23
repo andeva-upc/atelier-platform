7baa22ee7914ff9d05416c8e2b9fb0794ecb9b6e
xs4el <adiel.sanchez3110@gmail.com>
2026-05-23 17:10:00 -0500

    feat(inventory): add create product command

---
+++ src/main/java/com/andeva/atelier/platform/inventory/domain/model/commands/CreateProductCommand.java
@@ -0,0 +1,5 @@
+﻿package com.andeva.atelier.platform.inventory.domain.model.commands;
+import com.andeva.atelier.platform.inventory.domain.model.valueobjects.*;
+import com.andeva.atelier.platform.shared.domain.model.valueobjects.BranchId;
+import com.andeva.atelier.platform.shared.domain.model.valueobjects.Money;
+public record CreateProductCommand(BranchId branchId, ProductCategory category, ProductName name, Sku sku, String description, Money salePrice, InventoryQuantity minimumStock) {}
