fdf41af0689d7f166c0f4c531d6ca4817c81319d
xs4el <adiel.sanchez3110@gmail.com>
2026-05-24 17:30:00 -0500

    feat(inventory): add get products by branch id query

---
+++ src/main/java/com/andeva/atelier/platform/inventory/domain/model/queries/GetProductsByBranchIdQuery.java
@@ -0,0 +1,3 @@
+﻿package com.andeva.atelier.platform.inventory.domain.model.queries;
+import com.andeva.atelier.platform.shared.domain.model.valueobjects.BranchId;
+public record GetProductsByBranchIdQuery(BranchId branchId) {}
