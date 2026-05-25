a8e627f964c2017f6089cf75ec57317f88603b89
xs4el <adiel.sanchez3110@gmail.com>
2026-05-25 18:00:00 -0500

    feat(inventory): add auditable product batch jpa entity

---
+++ src/main/java/com/andeva/atelier/platform/inventory/infrastructure/persistence/jpa/entities/ProductBatchJpaEntity.java
@@ -0,0 +1,32 @@
+﻿package com.andeva.atelier.platform.inventory.infrastructure.persistence.jpa.entities;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.util.Date;
+import java.util.UUID;
+@Entity
+@Table(name = "inventory_product_batches")
+@EntityListeners(AuditingEntityListener.class)
+public class ProductBatchJpaEntity {
+    @Id
+    private UUID batchId;
+    private Integer initialQuantity;
+    private Integer availableQuantity;
+    private Double acquisitionCost;
+    @CreatedDate
+    private Date receptionDate;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductJpaEntity product;
+    public ProductBatchJpaEntity() {}
+    public UUID getBatchId() { return batchId; }
+    public void setBatchId(UUID batchId) { this.batchId = batchId; }
+    public Integer getInitialQuantity() { return initialQuantity; }
+    public void setInitialQuantity(Integer initialQuantity) { this.initialQuantity = initialQuantity; }
+    public Integer getAvailableQuantity() { return availableQuantity; }
+    public void setAvailableQuantity(Integer availableQuantity) { this.availableQuantity = availableQuantity; }
+    public Double getAcquisitionCost() { return acquisitionCost; }
+    public void setAcquisitionCost(Double acquisitionCost) { this.acquisitionCost = acquisitionCost; }
+    public ProductJpaEntity getProduct() { return product; }
+    public void setProduct(ProductJpaEntity product) { this.product = product; }
+}
