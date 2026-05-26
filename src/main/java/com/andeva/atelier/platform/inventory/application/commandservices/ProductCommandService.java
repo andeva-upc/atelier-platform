1f83435126df90faa9fdd1aaf6892ad0bb71ca6c
xs4el <adiel.sanchez3110@gmail.com>
2026-05-26 16:10:00 -0500

    feat(inventory): add product command service interface

---
+++ src/main/java/com/andeva/atelier/platform/inventory/application/commandservices/ProductCommandService.java
@@ -0,0 +1,5 @@
+﻿package com.andeva.atelier.platform.inventory.application.commandservices;
+import com.andeva.atelier.platform.inventory.domain.model.aggregates.Product;
+import com.andeva.atelier.platform.inventory.domain.model.commands.CreateProductCommand;
+import java.util.Optional;
+public interface ProductCommandService { Optional<Product> handle(CreateProductCommand command); }
