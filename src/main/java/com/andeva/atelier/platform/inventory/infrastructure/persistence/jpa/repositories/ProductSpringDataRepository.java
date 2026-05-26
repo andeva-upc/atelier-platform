5c95663e6080b4833af4e965a6b16c0c673bda49
xs4el <adiel.sanchez3110@gmail.com>
2026-05-26 09:20:00 -0500

    feat(inventory): add product spring data repository

---
+++ src/main/java/com/andeva/atelier/platform/inventory/infrastructure/persistence/jpa/repositories/ProductSpringDataRepository.java
@@ -0,0 +1,11 @@
+﻿package com.andeva.atelier.platform.inventory.infrastructure.persistence.jpa.repositories;
+import com.andeva.atelier.platform.inventory.infrastructure.persistence.jpa.entities.ProductJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.UUID;
+@Repository
+public interface ProductSpringDataRepository extends JpaRepository<ProductJpaEntity, UUID> {
+    boolean existsBySkuAndBranchId(String sku, String branchId);
+    List<ProductJpaEntity> findByBranchId(String branchId);
+}
