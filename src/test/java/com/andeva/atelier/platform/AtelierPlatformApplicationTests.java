4e4c34b0d1ffe057cbca9ed4d42c301c33c5dea9
shouydev <shouyestefanero@gmail.com>
2026-05-26 12:09:59 -0500

    Initial commit

---
+++ src/test/java/com/andeva/atelier/platform/AtelierPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andeva.atelier.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
