ecf19a382675aa5ce0ecdfa72d8a51dd18649b0a
xs4el <adiel.sanchez3110@gmail.com>
2026-05-27 09:10:00 -0500

    feat(inventory): add product query service interface

---
+++ src/main/java/com/andeva/atelier/platform/inventory/application/queryservices/ProductQueryService.java
@@ -0,0 +1,5 @@
+﻿package com.andeva.atelier.platform.inventory.application.queryservices;
+import com.andeva.atelier.platform.inventory.domain.model.aggregates.Product;
+import com.andeva.atelier.platform.inventory.domain.model.queries.GetProductByIdQuery;
+import java.util.Optional;
+public interface ProductQueryService { Optional<Product> handle(GetProductByIdQuery query); }
