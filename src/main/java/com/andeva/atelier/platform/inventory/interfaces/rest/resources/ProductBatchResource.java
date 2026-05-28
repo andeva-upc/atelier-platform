91b384fe589ccc2d6d3b41e4e5aa5cf3db4bdc15
xs4el <adiel.sanchez3110@gmail.com>
2026-05-28 10:30:00 -0500

    feat(inventory): add product batch resource dto

---
+++ src/main/java/com/andeva/atelier/platform/inventory/interfaces/rest/resources/ProductBatchResource.java
@@ -0,0 +1,3 @@
+﻿package com.andeva.atelier.platform.inventory.interfaces.rest.resources;
+import java.util.UUID;
+public record ProductBatchResource(UUID batchId, Integer availableQuantity, Double acquisitionCost) {}
