6608ff385476e5900107e84d485794a17574c998
xs4el <adiel.sanchez3110@gmail.com>
2026-05-28 15:40:00 -0500

    feat(inventory): add batch resource assembler

---
+++ src/main/java/com/andeva/atelier/platform/inventory/interfaces/rest/transform/ProductBatchResourceFromEntityAssembler.java
@@ -0,0 +1,8 @@
+﻿package com.andeva.atelier.platform.inventory.interfaces.rest.transform;
+import com.andeva.atelier.platform.inventory.domain.model.entities.ProductBatch;
+import com.andeva.atelier.platform.inventory.interfaces.rest.resources.ProductBatchResource;
+public class ProductBatchResourceFromEntityAssembler {
+    public static ProductBatchResource toResourceFromEntity(ProductBatch batch) {
+        return new ProductBatchResource(batch.getBatchId(), batch.getAvailableQuantity().value(), batch.getAcquisitionCost().amount());
+    }
+}
