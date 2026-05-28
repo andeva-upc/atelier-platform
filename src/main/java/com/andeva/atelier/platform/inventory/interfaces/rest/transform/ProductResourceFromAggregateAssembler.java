de58292352f9719c6f7224c63230150ba59bb103
xs4el <adiel.sanchez3110@gmail.com>
2026-05-28 12:05:00 -0500

    feat(inventory): add resource from aggregate assembler

---
+++ src/main/java/com/andeva/atelier/platform/inventory/interfaces/rest/transform/ProductResourceFromAggregateAssembler.java
@@ -0,0 +1,8 @@
+﻿package com.andeva.atelier.platform.inventory.interfaces.rest.transform;
+import com.andeva.atelier.platform.inventory.domain.model.aggregates.Product;
+import com.andeva.atelier.platform.inventory.interfaces.rest.resources.ProductResource;
+public class ProductResourceFromAggregateAssembler {
+    public static ProductResource toResourceFromAggregate(Product product) {
+        return new ProductResource(product.getId(), product.getBranchId().value(), product.getCategory().name(), product.getName().name(), product.getSku().value(), product.getCurrentStock().value(), product.getReservedStock().value());
+    }
+}
