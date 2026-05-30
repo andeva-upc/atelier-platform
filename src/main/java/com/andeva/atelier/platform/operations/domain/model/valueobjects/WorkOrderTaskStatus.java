f8dd7a589b3620884c3b651a5ac3665b4fcf1659
shouydev <shouyestefanero@gmail.com>
2026-05-29 23:11:23 -0500

    style(operations): enhance value objects with detailed documentation and validation.

--- src/main/java/com/andeva/atelier/platform/operations/domain/model/valueobjects/WorkOrderTaskStatus.java
+++ src/main/java/com/andeva/atelier/platform/operations/domain/model/valueobjects/WorkOrderTaskStatus.java
@@ -1,5 +1,10 @@
 package com.andeva.atelier.platform.operations.domain.model.valueobjects;
 
+/**
+ * Enum representing the status of a Work Order Task, with defined transitions between states.
+ * The possible statuses are PENDING, DOING, and COMPLETED, each with specific rules for transitioning to the next status.
+ * @author Joel Huamani Estefanero
+ */
 public enum WorkOrderTaskStatus {
     PENDING {
         @Override
@@ -22,6 +27,11 @@ public enum WorkOrderTaskStatus {
 
     private static final String INVALID_TRANSITION_MESSAGE_WORK_ORDER_TASK = "operations.error.workOrderTaskStatus.invalidTransition";
 
+    /**
+     * Determines if the current status can transition to the specified next status based on defined rules for each status.
+     * @param next the next WorkOrderTaskStatus to transition to other enum status
+     * @return true if the transition is valid according to the rules defined for each status, false otherwise
+     */
     public abstract boolean canTransitionTo(WorkOrderTaskStatus next);
 
     public WorkOrderTaskStatus transitionTo(WorkOrderTaskStatus next) {
