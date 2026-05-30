1111f2681be9576d8f3759d035504b666ae25d7d
shouydev <shouyestefanero@gmail.com>
2026-05-29 23:09:43 -0500

    style(shared): enhance jpa auditing configuration with detailed documentation for auditor provider.

--- src/main/java/com/andeva/atelier/platform/shared/infrastructure/persistence/jpa/configuration/AuditConfiguration.java
+++ src/main/java/com/andeva/atelier/platform/shared/infrastructure/persistence/jpa/configuration/AuditConfiguration.java
@@ -9,12 +9,19 @@ import java.util.Optional;
 import java.util.UUID;
 
 /**
- * Implements with Spring Security
+ * Configuration class for setting up JPA auditing in the application.
+ * This class defines a bean for providing the current auditor (user) information.
+ * The auditor information is typically used to automatically populate auditing fields such as createdBy and lastModifiedBy in JPA entities.
+ * In this example, the auditor is represented as a UUID, and a default value is returned for demonstration purposes. In a real application, you would replace this with logic to retrieve the actual user information from the security context or authentication mechanism in use (e.g., Spring Security).
+ * @author Joel Huamani Estefanero
  */
-
 @Configuration
 public class AuditConfiguration {
 
+    /**
+     * Defines a bean that provides the current auditor information for JPA auditing. The auditor is represented as a UUID, and a default value is returned for demonstration purposes. In a real application, you would replace this with logic to retrieve the actual user information from the security context or authentication mechanism in use (e.g., Spring Security).
+     * @return An instance of AuditorAware that provides the current auditor information as a UUID.
+     */
     @Bean
     public AuditorAware<UUID> auditorProvider() {
         return () -> {
