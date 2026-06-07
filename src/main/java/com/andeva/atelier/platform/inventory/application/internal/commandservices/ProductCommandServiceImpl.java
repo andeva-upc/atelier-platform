31b2675b5b62ba1e167b7f5f2e4e04a9ca9a63cb
Adiel Sanchez <adiel.sanchez3110@gmail.com>
2026-06-07 14:15:00 -0500

    feat(inventory): add ProductCommandService and implementation

---
+++ src/main/java/com/andeva/atelier/platform/inventory/application/internal/commandservices/ProductCommandServiceImpl.java
@@ -0,0 +1,26 @@
+package com.andeva.atelier.platform.inventory.application.internal.commandservices;
+
+import com.andeva.atelier.platform.inventory.application.commandservices.ProductCommandService;
+import com.andeva.atelier.platform.inventory.domain.model.aggregates.Product;
+import com.andeva.atelier.platform.inventory.domain.model.commands.CreateProductCommand;
+import com.andeva.atelier.platform.inventory.domain.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ProductCommandServiceImpl implements ProductCommandService {
+    private final ProductRepository productRepository;
+
+    public ProductCommandServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Optional<Product> handle(CreateProductCommand command) {
+        Product product = new Product(UUID.randomUUID(), command.branchId(), command.category(), command.name(), command.sku());
+        productRepository.save(product);
+        return Optional.of(product);
+    }
+}
