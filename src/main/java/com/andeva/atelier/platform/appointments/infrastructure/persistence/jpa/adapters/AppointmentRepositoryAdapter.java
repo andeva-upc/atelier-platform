a8e762625a401de021a346ca6f4f01242dda4055
Jennifer <u20241c998@upc.edu.pe>
2026-06-08 03:26:22 -0500

    feat(appointments): add appointment repository adapter.

---
+++ src/main/java/com/andeva/atelier/platform/appointments/infrastructure/persistence/jpa/adapters/AppointmentRepositoryAdapter.java
@@ -0,0 +1,24 @@
+package com.andeva.atelier.platform.appointments.infrastructure.persistence.jpa.adapters;
+
+import com.andeva.atelier.platform.appointments.domain.model.aggregates.Appointment;
+import com.andeva.atelier.platform.appointments.domain.repositories.AppointmentRepository;
+import com.andeva.atelier.platform.appointments.infrastructure.persistence.jpa.assemblers.AppointmentPersistenceAssembler;
+import com.andeva.atelier.platform.appointments.infrastructure.persistence.jpa.repositories.AppointmentJpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppointmentRepositoryAdapter implements AppointmentRepository {
+
+    private final AppointmentJpaRepository appointmentJpaRepository;
+
+    public AppointmentRepositoryAdapter(AppointmentJpaRepository appointmentJpaRepository) {
+        this.appointmentJpaRepository = appointmentJpaRepository;
+    }
+
+    @Override
+    public Appointment save(Appointment appointment) {
+        var entity = AppointmentPersistenceAssembler.toEntityFromAggregate(appointment);
+        var savedEntity = appointmentJpaRepository.save(entity);
+        return AppointmentPersistenceAssembler.toAggregateFromEntity(savedEntity);
+    }
+}
