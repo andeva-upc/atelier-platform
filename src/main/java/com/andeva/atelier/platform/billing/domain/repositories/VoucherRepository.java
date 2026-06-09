d1b8734fda590107e26d5b4e85192b66a67e868a
AldoDev20 <u202419485@upc.edu.pe>
2026-06-08 23:00:18 -0500

    feat(billing): create GetVouchersByBranchIdQuery and update domain repository.

--- src/main/java/com/andeva/atelier/platform/billing/domain/repositories/VoucherRepository.java
+++ src/main/java/com/andeva/atelier/platform/billing/domain/repositories/VoucherRepository.java
@@ -4,8 +4,11 @@ import com.andeva.atelier.platform.billing.domain.model.aggregates.Voucher;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
+import com.andeva.atelier.platform.shared.domain.model.valueobjects.BranchId;
 
 public interface VoucherRepository {
     Voucher save(Voucher voucher);
     Optional<Voucher> findById(UUID id);
+    List<Voucher> findByBranchId(BranchId branchId);
 }
