1cbc6b3dde4e1e4640de0e515b5760de0633daf0
Jennifer <u20241c998@upc.edu.pe>
2026-06-09 03:37:04 -0500

    feat(appointments): add repository support for appointment update

--- src/main/java/com/andeva/atelier/platform/appointments/domain/repositories/AppointmentRepository.java
+++ src/main/java/com/andeva/atelier/platform/appointments/domain/repositories/AppointmentRepository.java
@@ -3,18 +3,27 @@ package com.andeva.atelier.platform.appointments.domain.repositories;
 import com.andeva.atelier.platform.appointments.domain.model.aggregates.Appointment;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AppointmentRepository {
 
     Appointment save(Appointment appointment);
 
+    Optional<Appointment> findById(UUID appointmentId);
+
+    boolean existsById(UUID appointmentId);
+
+    void deleteById(UUID appointmentId);
+
     boolean existsByScheduledStartLessThanAndScheduledEndGreaterThan(
             LocalDateTime scheduledEnd,
             LocalDateTime scheduledStart
     );
 
-    boolean existsById(UUID appointmentId);
-
-    void deleteById(UUID appointmentId);
+    boolean existsByIdNotAndScheduledStartLessThanAndScheduledEndGreaterThan(
+            UUID appointmentId,
+            LocalDateTime scheduledEnd,
+            LocalDateTime scheduledStart
+    );
 }
