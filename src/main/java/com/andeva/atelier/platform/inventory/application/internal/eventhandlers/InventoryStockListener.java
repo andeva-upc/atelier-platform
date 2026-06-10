27e125b48cb591d18a9de987b167225303db4b7d
shouydev <shouyestefanero@gmail.com>
2026-06-10 04:33:42 -0500

    refactor(inventory): remove unused import for work order task product.

--- src/main/java/com/andeva/atelier/platform/inventory/application/internal/eventhandlers/InventoryStockListener.java
+++ src/main/java/com/andeva/atelier/platform/inventory/application/internal/eventhandlers/InventoryStockListener.java
@@ -3,7 +3,6 @@ package com.andeva.atelier.platform.inventory.application.internal.eventhandlers
 import com.andeva.atelier.platform.inventory.domain.model.aggregates.Product;
 import com.andeva.atelier.platform.inventory.domain.model.valueobjects.InventoryQuantity;
 import com.andeva.atelier.platform.inventory.domain.repositories.ProductRepository;
-import com.andeva.atelier.platform.operations.domain.model.aggregates.WorkOrderTaskProduct;
 import com.andeva.atelier.platform.operations.domain.model.events.ProductReservationCanceledEvent;
 import com.andeva.atelier.platform.operations.domain.model.events.ProductReservedEvent;
 import com.andeva.atelier.platform.operations.domain.model.events.WorkOrderPaidEvent;
