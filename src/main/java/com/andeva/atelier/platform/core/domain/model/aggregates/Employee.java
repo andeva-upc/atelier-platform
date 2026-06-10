9aeaa84ced360d246952c8a77ce14fccab08d26e
shouydev <shouyestefanero@gmail.com>
2026-06-09 21:50:40 -0500

    refactor(core): simplify employee constructors and update repository references.

--- src/main/java/com/andeva/atelier/platform/core/domain/model/aggregates/Employee.java
+++ src/main/java/com/andeva/atelier/platform/core/domain/model/aggregates/Employee.java
@@ -6,7 +6,9 @@ import com.andeva.atelier.platform.core.domain.model.valueobjects.PersonName;
 import com.andeva.atelier.platform.core.domain.model.valueobjects.Phone;
 import com.andeva.atelier.platform.core.domain.model.valueobjects.UserId;
 import com.andeva.atelier.platform.shared.domain.model.aggregates.AbstractDomainAggregateRoot;
+
 import lombok.Getter;
+import java.util.UUID;
 
 @Getter
 public class Employee extends AbstractDomainAggregateRoot<Employee> {
@@ -17,19 +19,21 @@ public class Employee extends AbstractDomainAggregateRoot<Employee> {
     private Document document;
     private Phone phone;
 
-    public Employee() {
-    }
+    public Employee() {}
 
-    public Employee(UserId userId, PersonName name, Document document, Phone phone) {
+    public Employee(EmployeeId id, UserId userId, PersonName name, Document document, Phone phone) {
+        this.id = id;
         this.userId = userId;
-        this.name = name;
         this.document = document;
         this.phone = phone;
     }
 
-    public Employee(EmployeeId id, UserId userId, PersonName name, Document document, Phone phone) {
-        this(userId, name, document, phone);
-        this.id = id;
+    public Employee(UserId userId, PersonName name, Document document, Phone phone) {
+        this.id = new EmployeeId(UUID.randomUUID());
+        this.userId = userId;
+        this.name = name;
+        this.document = document;
+        this.phone = phone;
     }
 
     public void update(PersonName name, Document document, Phone phone) {
