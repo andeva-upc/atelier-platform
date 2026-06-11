0ef42245c663da673d335bd89abdb51c49c1efa1
danieltyuyu <di0014617@gmail.com>
2026-06-11 01:02:35 -0500

    feat(iot): implement handle for telemetry by registration id query.

--- src/main/java/com/andeva/atelier/platform/iot/application/queryservices/TelemetryQueryService.java
+++ src/main/java/com/andeva/atelier/platform/iot/application/queryservices/TelemetryQueryService.java
@@ -3,6 +3,7 @@ package com.andeva.atelier.platform.iot.application.queryservices;
 import com.andeva.atelier.platform.iot.domain.model.aggregates.TelemetrySnapshot;
 import com.andeva.atelier.platform.iot.domain.model.queries.GetLatestTelemetrySnapshotQuery;
 import com.andeva.atelier.platform.iot.domain.model.queries.GetTelemetrySnapshotHistoryQuery;
+import com.andeva.atelier.platform.iot.domain.model.queries.GetTelemetrySnapshotsByRegistrationIdQuery;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,4 +26,11 @@ public interface TelemetryQueryService {
      * @return the list of snapshots ordered by creation date descending
      */
     List<TelemetrySnapshot> handle(GetTelemetrySnapshotHistoryQuery query);
+
+    /**
+     * Handles retrieving the complete history of telemetry snapshots for a specific OBD2 device registration.
+     * @param query the query containing the registration identifier
+     * @return the list of snapshots ordered by creation date descending
+     */
+    List<TelemetrySnapshot> handle(GetTelemetrySnapshotsByRegistrationIdQuery query);
 }
