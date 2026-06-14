351f4ab6efe1853446b8480a18d20d3698817033
shouydev <shouyestefanero@gmail.com>
2026-06-14 17:43:13 -0500

    feat(core): add method to retrieve owner by user id in owner query service impl.

--- src/main/java/com/andeva/atelier/platform/core/application/internal/queryservices/OwnerQueryServiceImpl.java
+++ src/main/java/com/andeva/atelier/platform/core/application/internal/queryservices/OwnerQueryServiceImpl.java
@@ -3,6 +3,7 @@ package com.andeva.atelier.platform.core.application.internal.queryservices;
 import com.andeva.atelier.platform.core.application.queryservices.OwnerQueryService;
 import com.andeva.atelier.platform.core.domain.model.aggregates.Owner;
 import com.andeva.atelier.platform.core.domain.model.queries.GetOwnerByIdQuery;
+import com.andeva.atelier.platform.core.domain.model.queries.GetOwnerByUserIdQuery;
 import com.andeva.atelier.platform.core.domain.repositories.OwnerRepository;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +20,10 @@ public class OwnerQueryServiceImpl implements OwnerQueryService {
     @Override
     public Optional<Owner> handle(GetOwnerByIdQuery query) {
         return ownerRepository.findById(query.id());
+    }
+
+    @Override
+    public Optional<Owner> handle(GetOwnerByUserIdQuery query) {
+        return ownerRepository.findByUserId(query.userId());
     }
 }
