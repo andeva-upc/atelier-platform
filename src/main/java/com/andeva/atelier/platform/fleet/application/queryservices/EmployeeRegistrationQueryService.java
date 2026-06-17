ffa0e11899e7dfc195a45fdf371d44e67b8c6838
AldoDev20 <u202419485@upc.edu.pe>
2026-06-17 13:15:50 -0500

    feat(fleet): implement EmployeeRegistrationQueryService for GET by status.

--- src/main/java/com/andeva/atelier/platform/fleet/application/queryservices/EmployeeRegistrationQueryService.java
+++ src/main/java/com/andeva/atelier/platform/fleet/application/queryservices/EmployeeRegistrationQueryService.java
@@ -3,6 +3,7 @@ package com.andeva.atelier.platform.fleet.application.queryservices;
 import com.andeva.atelier.platform.fleet.domain.model.aggregates.EmployeeRegistration;
 import com.andeva.atelier.platform.fleet.domain.model.queries.GetEmployeeRegistrationByIdQuery;
 import com.andeva.atelier.platform.fleet.domain.model.queries.GetEmployeeRegistrationsByBranchIdQuery;
+import com.andeva.atelier.platform.fleet.domain.model.queries.GetEmployeeRegistrationsByBranchIdAndStatusQuery;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface EmployeeRegistrationQueryService {
     Optional<EmployeeRegistration> handle(GetEmployeeRegistrationByIdQuery query);
     List<EmployeeRegistration> handle(GetEmployeeRegistrationsByBranchIdQuery query);
+    List<EmployeeRegistration> handle(GetEmployeeRegistrationsByBranchIdAndStatusQuery query);
 }
