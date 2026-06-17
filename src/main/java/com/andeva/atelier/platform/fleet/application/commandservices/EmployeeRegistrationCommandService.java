160540048f72afcdadb196ff9bb776af91f2a67d
Patto04 <morochomariana827@gmail.com>
2026-06-17 09:15:46 -0500

    feat(fleet): add command and query services for employee registration handling.

---
+++ src/main/java/com/andeva/atelier/platform/fleet/application/commandservices/EmployeeRegistrationCommandService.java
@@ -0,0 +1,9 @@
+package com.andeva.atelier.platform.fleet.application.commandservices;
+
+import com.andeva.atelier.platform.fleet.domain.model.aggregates.EmployeeRegistration;
+import com.andeva.atelier.platform.fleet.domain.model.commands.CreateEmployeeRegistrationCommand;
+import com.andeva.atelier.platform.shared.application.result.Result;
+
+public interface EmployeeRegistrationCommandService {
+    Result<EmployeeRegistration, EmployeeRegistrationCommandFailure> handle(CreateEmployeeRegistrationCommand command);
+}
