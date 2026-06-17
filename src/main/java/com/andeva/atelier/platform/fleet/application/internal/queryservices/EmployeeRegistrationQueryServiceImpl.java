62cb6ff2c74a266c06689e517c85b967d9f2a8cf
AldoDev20 <u202419485@upc.edu.pe>
2026-06-17 12:48:36 -0500

    feat(fleet): implement EmployeeRegistrationQueryService for GET by branchId.

--- src/main/java/com/andeva/atelier/platform/fleet/application/internal/queryservices/EmployeeRegistrationQueryServiceImpl.java
+++ src/main/java/com/andeva/atelier/platform/fleet/application/internal/queryservices/EmployeeRegistrationQueryServiceImpl.java
@@ -3,9 +3,11 @@ package com.andeva.atelier.platform.fleet.application.internal.queryservices;
 import com.andeva.atelier.platform.fleet.application.queryservices.EmployeeRegistrationQueryService;
 import com.andeva.atelier.platform.fleet.domain.model.aggregates.EmployeeRegistration;
 import com.andeva.atelier.platform.fleet.domain.model.queries.GetEmployeeRegistrationByIdQuery;
+import com.andeva.atelier.platform.fleet.domain.model.queries.GetEmployeeRegistrationsByBranchIdQuery;
 import com.andeva.atelier.platform.fleet.domain.repositories.EmployeeRegistrationRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +22,10 @@ public class EmployeeRegistrationQueryServiceImpl implements EmployeeRegistratio
     @Override
     public Optional<EmployeeRegistration> handle(GetEmployeeRegistrationByIdQuery query) {
         return employeeRegistrationRepository.findById(query.id());
+    }
+
+    @Override
+    public List<EmployeeRegistration> handle(GetEmployeeRegistrationsByBranchIdQuery query) {
+        return employeeRegistrationRepository.findByBranchId(query.branchId());
     }
 }
--- src/main/java/com/andeva/atelier/platform/fleet/application/queryservices/EmployeeRegistrationQueryService.java
+++ src/main/java/com/andeva/atelier/platform/fleet/application/queryservices/EmployeeRegistrationQueryService.java
@@ -2,9 +2,12 @@ package com.andeva.atelier.platform.fleet.application.queryservices;
 
 import com.andeva.atelier.platform.fleet.domain.model.aggregates.EmployeeRegistration;
 import com.andeva.atelier.platform.fleet.domain.model.queries.GetEmployeeRegistrationByIdQuery;
+import com.andeva.atelier.platform.fleet.domain.model.queries.GetEmployeeRegistrationsByBranchIdQuery;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRegistrationQueryService {
     Optional<EmployeeRegistration> handle(GetEmployeeRegistrationByIdQuery query);
+    List<EmployeeRegistration> handle(GetEmployeeRegistrationsByBranchIdQuery query);
 }
