859a1c94080deac0600c4254a7abc11a671033f7
AldoDev20 <u202419485@upc.edu.pe>
2026-06-21 12:03:56 -0500

    feat(core): add employee search by document number.

--- src/main/java/com/andeva/atelier/platform/core/application/queryservices/EmployeeQueryService.java
+++ src/main/java/com/andeva/atelier/platform/core/application/queryservices/EmployeeQueryService.java
@@ -4,10 +4,12 @@ import com.andeva.atelier.platform.core.domain.model.aggregates.Employee;
 import com.andeva.atelier.platform.core.domain.model.queries.GetEmployeeByIdQuery;
 
 import com.andeva.atelier.platform.core.domain.model.queries.GetEmployeeByUserIdQuery;
+import com.andeva.atelier.platform.core.domain.model.queries.GetEmployeeByDocumentNumberQuery;
 
 import java.util.Optional;
 
 public interface EmployeeQueryService {
     Optional<Employee> handle(GetEmployeeByIdQuery query);
     Optional<Employee> handle(GetEmployeeByUserIdQuery query);
+    Optional<Employee> handle(GetEmployeeByDocumentNumberQuery query);
 }
