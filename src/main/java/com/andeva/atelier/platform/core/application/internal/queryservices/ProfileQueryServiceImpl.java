cfad3c46d26ec6789a2cb5b80c36623484722113
AldoDev20 <u202419485@upc.edu.pe>
2026-06-20 23:39:10 -0500

    fix(core): resolve value object mapping errors in profile query.

--- src/main/java/com/andeva/atelier/platform/core/application/internal/queryservices/ProfileQueryServiceImpl.java
+++ src/main/java/com/andeva/atelier/platform/core/application/internal/queryservices/ProfileQueryServiceImpl.java
@@ -52,19 +52,21 @@ public class ProfileQueryServiceImpl implements ProfileQueryService {
         var customer = customerRepository.findByDocumentNumber(query.documentNumber());
         if (customer.isPresent()) {
             var c = customer.get();
-            return Optional.of(new ProfileSummary(c.getUserId().value(), c.getFirstName(), c.getLastName(), c.getDocument().getDocumentType(), c.getDocument().getDocumentNumber(), "CUSTOMER"));
+            String firstName = c.getName() != null ? c.getName().firstName() : c.getBusinessName();
+            String lastName = c.getName() != null ? c.getName().lastName() : "";
+            return Optional.of(new ProfileSummary(c.getUserId().value(), firstName, lastName, c.getDocument().getDocumentType().name(), c.getDocument().getDocumentNumber(), "CUSTOMER"));
         }
 
         var employee = employeeRepository.findByDocumentNumber(query.documentNumber());
         if (employee.isPresent()) {
             var e = employee.get();
-            return Optional.of(new ProfileSummary(e.getUserId().value(), e.getFirstName(), e.getLastName(), e.getDocument().getDocumentType(), e.getDocument().getDocumentNumber(), "EMPLOYEE"));
+            return Optional.of(new ProfileSummary(e.getUserId().value(), e.getName().firstName(), e.getName().lastName(), e.getDocument().getDocumentType().name(), e.getDocument().getDocumentNumber(), "EMPLOYEE"));
         }
 
         var owner = ownerRepository.findByDocumentNumber(query.documentNumber());
         if (owner.isPresent()) {
             var o = owner.get();
-            return Optional.of(new ProfileSummary(o.getUserId().value(), o.getFirstName(), o.getLastName(), o.getDocument().getDocumentType(), o.getDocument().getDocumentNumber(), "OWNER"));
+            return Optional.of(new ProfileSummary(o.getUserId().value(), o.getName().firstName(), o.getName().lastName(), o.getDocument().getDocumentType().name(), o.getDocument().getDocumentNumber(), "OWNER"));
         }
 
         return Optional.empty();
