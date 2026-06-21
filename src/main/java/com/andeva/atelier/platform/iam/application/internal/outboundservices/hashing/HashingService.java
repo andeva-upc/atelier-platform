5113c81328c63c25468b4fc7a7f88c8377ed527c
shouydev <shouyestefanero@gmail.com>
2026-06-20 20:40:10 -0500

    feat(iam): implement custom unauthorized request handler and refactor authentication token processing.

--- src/main/java/com/andeva/atelier/platform/iam/application/internal/outboundservices/hashing/HashingService.java
+++ src/main/java/com/andeva/atelier/platform/iam/application/internal/outboundservices/hashing/HashingService.java
@@ -2,5 +2,6 @@ package com.andeva.atelier.platform.iam.application.internal.outboundservices.ha
 
 public interface HashingService {
     String encode(CharSequence rawPassword);
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     boolean matches(CharSequence rawPassword, String encodedPassword);
 }
