49cde544524eed17259dbd6e34894c615fd146e9
shouydev <shouyestefanero@gmail.com>
2026-06-20 20:43:27 -0500

    feat(iam): implement bearer token service and token service impl for jwt token management.

---
+++ src/main/java/com/andeva/atelier/platform/iam/infrastructure/tokens/jwt/BearerTokenService.java
@@ -0,0 +1,10 @@
+package com.andeva.atelier.platform.iam.infrastructure.tokens.jwt;
+
+import com.andeva.atelier.platform.iam.application.internal.outboundservices.tokens.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+
+public interface BearerTokenService extends TokenService {
+    String getBearerTokenFrom(HttpServletRequest request);
+    String generateToken(Authentication authentication);
+}
